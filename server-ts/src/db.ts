d06f1079a7b5627919fc287a770b83f9334a2792
Phattarakorn_Nalinbenchaphan <6510110342@psu.ac.th>
2023-01-27 20:18:53 +0700

    set table

---
+++ server-ts/src/db.ts
@@ -0,0 +1,10 @@
+import Knex from 'knex'
+import appConfig from "./config";
+
+const knexStringCase = require('knex-stringcase')
+
+const db = Knex(knexStringCase({
+    client: "mysql2",
+    connection: appConfig.dbconnectinfo,
+    useNullAsDefalult: true
+}))
