d8f5d18d4a05bb1ebe670dbb8cd3293586ce8d9f
Phattarakorn_Nalinbenchaphan <6510110342@psu.ac.th>
2023-01-28 14:47:24 +0700

    use repo

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import './App.css';
+import Repo from './repositories'
+import { useEffect ,useState } from 'react';
+import Announcement from './models/Announcement';
 
 function App() {
+  const [announcementList, setAnnouncementList] = useState<Announcement[]>([])
+
+  const fetchannouncementList = async () => {
+    const result = await Repo.announcements.getAll()
+    if(result){
+      setAnnouncementList(result)
+    }
+  }
+
+  useEffect(() => {
+    fetchannouncementList()
+  })
+
   return (
-    <div className="App">
-      "Hello world"
+    <div>
+      {announcementList.map(announcement => (
+        <div key={announcement.id}>
+          <p>ID : {announcement.id}</p>
+          <p>Description : {announcement.description}</p>
+          <p>Positive : {announcement.remarkIfPositive}</p>
+          <p>Negative : {announcement.remarkIfNegative}</p>
+          <p>Time : {announcement.pubDateTime.toLocaleString('en-US', {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit',
+              hour: '2-digit',
+              minute: '2-digit',
+              second: '2-digit'
+            })}</p>
+          <p>UserCode : {announcement.userCode}</p>
+        </div>
+      ))}
     </div>
   );
 }
