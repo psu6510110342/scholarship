2f655b8454ea69d6b0bf92b563a22a39b41dd5b5
Phattarakorn_Nalinbenchaphan <6510110342@psu.ac.th>
2023-01-28 09:58:05 +0700

    use databases from mysql

--- server-ts/src/api/announcement.ts
+++ server-ts/src/api/announcement.ts
@@ -1,9 +1,15 @@
 import Router from "koa-router";
+import db from '../db'
 
 const router = new Router()
 
-router.get('/', async (ctx, next) => {
-    ctx.body = {msg: 'Hello world from announcement api'}
-    })    
+.get('/', async (ctx,next) => {
+    let query = db('announcement').select('*')
+    if (ctx.request.query['keyword']) {
+        const keyword = String(ctx.request.query['keyword'])
+        query = query.where((it) => {it.where('topic','like', `%${keyword}`).orWhere('description','like',`%${keyword}`)})
+    }
+    ctx.body = await query.orderBy('id','desc')
+})
 
 export default router
--- server-ts/src/api/user_result.ts
+++ server-ts/src/api/user_result.ts
@@ -1,9 +1,28 @@
 import Router from "koa-router";
+import db from '../db'
+import { nestObject } from "./utils";
 
 const router = new Router()
 
-router.get('/', async (ctx, next) => {
-    ctx.body = {msg: 'Hello world from userResult api'}
-    })    
+const makeQuery = () => db('userResult').select('*')
+
+router
+  .get('/', async (ctx, next) => {
+    let query = makeQuery()
+    if (ctx.request.query['announcementId']) {
+      const announcementId = Number(ctx.request.query['announcementId'])
+      query = query.where({ announcementId })
+    }
+    if (ctx.request.query['isPinned']) {
+      const isPinned = Boolean(ctx.request.query['isPinned'])
+      query = query.where({ isPinned })
+    }
+    if (ctx.request.query['keyword']) {
+      const keyword = String(ctx.request.query['keyword'])
+      query = query.where((it) => {it.where('announcement.topic', 'like', `%${keyword}%`).orWhere('announcement.description', 'like', `%${keyword}%`)})
+    }
+    const userResults = await query.orderBy('id', 'desc')
+    ctx.body = userResults.map(it => nestObject(it, 'announcement'))
+  })
 
 export default router
