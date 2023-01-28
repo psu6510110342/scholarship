b153fc0af13d8a822b93d0d7b07d37e5b8fad341
Phattarakorn_Nalinbenchaphan <6510110342@psu.ac.th>
2023-01-28 10:10:08 +0700

    add announcement method

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,13 +1,16 @@
 import Koa from 'koa'
 import json from 'koa-json'
+import { koaBody } from 'koa-body'
 
 import apiRouter from './api'
 import appConfig from './config'
 import loadFixtures from './fixtures'
 
+
 const app = new Koa()
 
 app.use(json())
+app.use(koaBody())
 app.use(apiRouter.routes())
 
 app.listen(8000)
