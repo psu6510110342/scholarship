c88eb3c23c8da9a0de0d21536ff016e9ef3be038
Phattarakorn_Nalinbenchaphan <6510110342@psu.ac.th>
2023-01-28 17:57:16 +0700

    use route

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,64 +1,17 @@
-import Repo from './repositories'
-import { useEffect ,useState } from 'react';
-import Announcement from './models/Announcement';
-import UserResult from './models/UserResult';
+import './App.css';
+import AppRoutes from './config/routes';
+import { AuthProvider } from "react-oidc-context";
+import { oidcConfig } from './config';
+import AppProvider from './AppProvider';
 
 function App() {
-  const [announcementList, setAnnouncementList] = useState<Announcement[]>([])
-  const [userResultList, setuserResultList] = useState<UserResult[]>([])
-
-  const fetchannouncementList = async () => {
-    const result = await Repo.announcements.getAll()
-    if(result){
-      setAnnouncementList(result)
-    }
-  }
-
-  const fetchuserResultList = async () => {
-    const result = await Repo.userResults.getAll()
-    if(result){
-      setuserResultList(result)
-    }
-  }
-
-
-  useEffect(() => {
-    fetchannouncementList()
-    fetchuserResultList()
-  })
-
   return (
-    <div>
-      {announcementList.map(announcement => (
-        <div key={announcement.id}>
-          <p>ID : {announcement.id}</p>
-          <p>Description : {announcement.description}</p>
-          <p>Positive : {announcement.remarkIfPositive}</p>
-          <p>Negative : {announcement.remarkIfNegative}</p>
-          <p>Time : {announcement.pubDateTime.toLocaleString('en-US', {
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit',
-              second: '2-digit'
-            })}</p>
-          <p>UserCode : {announcement.userCode}</p>
-        </div>
-      ))}
-      <hr />
-      <div>
-        {userResultList.map(userResult => (
-          <div key={userResult.id}>
-            <p>ID : {userResult.id}</p>
-            <p>Topic : {userResult.announcement.topic}</p>
-            <p>Result : {userResult.result}</p>
-            <p>UserCode : {userResult.userCode}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    <AuthProvider {...oidcConfig}>
+      <AppProvider>
+        <AppRoutes />
+      </AppProvider>      
+    </AuthProvider>
+  )
 }
 
 export default App;
